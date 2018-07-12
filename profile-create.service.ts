f1d01fabb225d248c558662ed67e2e7dfa4916a4
Jitendra Rajpurohit <rajpurohitjitendra81@gmail.com>
2018-07-12 16:59:18 +0530

    Delete profile-create.service.ts

--- profile-create.service.ts
+++
@@ -1,9 +0,0 @@
- postFile(fileToUpload: File): Observable<boolean> {
-    const endpoint = 'http://localhost:3000/profile/upload';
-    const formData: FormData = new FormData();
-    formData.append('fileKey', fileToUpload, fileToUpload.name);
-    return this.httpClient
-      .post(endpoint, formData)
-      .map(() => { return true; })
-      .catch((e) => this.handleError(e));
-}
